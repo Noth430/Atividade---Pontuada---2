b99a969be89cc770ef7252b5fd56ca8a7e4bf618
GabrielDV2004 <gabrielsantosgpt04@gmail.com>
2024-09-23 19:39:56 +0000

    salvar

---
+++ Cod Biel.py
@@ -0,0 +1,26 @@
+"""
+Informe o número da turma: 
+Turma - G93313
+
+Nome completo dos componentes.
+1 - Gabriel Pinto dos Santos
+2 - Henrique Santos Silva
+"""
+
+
+import os
+
+# Limpa o terminal.
+os.system("cls || clear") 
+
+print("""
+escreva o código do prato para fazer o pedido:
+codigo |Menu              |Valor |
+1      |Picanha           |100,00|
+2      |Strognoff         |35,00 |
+3      |Lasanha           |25,00 |
+4      |Frango a Milanesa |70,00 |
+5      |Peixe Grelhado    |60,00 |  
+6      |Burrito com Fritas|85,00 |
+7      |Hamburguer        |25,00 |  
+      """)
