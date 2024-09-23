50344c07a978c634ae30d2acd02accaedc90da33
GabrielDV2004 <gabrielsantosgpt04@gmail.com>
2024-09-23 19:47:27 +0000

    salvar

--- Cod Biel.py
+++ Cod Biel.py
@@ -24,3 +24,30 @@ codigo |Menu              |Valor |
 6      |Burrito com Fritas|85,00 |
 7      |Hamburguer        |25,00 |  
       """)
+
+Codigo_Prato =(input("Digite o Código do seu Prato: "))
+
+
+
+resultado = 0
+
+match(Codigo_Prato):
+    case "1":
+        print("Picanha R$ 100,00")
+    case "2":
+        print("Strognoff R$35,00")
+    case "3":
+        print("Lasanha R$35,00")
+    case "4":
+        print("Frango a Milanesa R$70,00")
+    case "5":
+        print("Peixe Grelhado R$60,00")
+    case "6":
+        print("Burrito com Fritas R$85,00")
+    case "7":
+        print("Hamburguer R$25,00")
+    case "0":
+        print("Opção invalida, Por favor digite um Código Valido.")
+
+
+print,("=== FIM ===")      
